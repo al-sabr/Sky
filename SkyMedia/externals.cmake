1275078e446a86cd720382ff27c8af37a3927409
Ramazan Ergüder Bekrek <e.bekrek@yandex.com>
2021-05-25 18:07:09 +0200

    Removed all VLC internal files from the build tree.

--- SkyMedia/externals.cmake
+++
@@ -1,7 +0,0 @@
-add_library(libvlc STATIC IMPORTED)
-set_property(TARGET libvlc PROPERTY
-IMPORTED_LOCATION "${CMAKE_SOURCE_DIR}/vlc/libvlc.lib")
-
-add_library(libvlccore STATIC IMPORTED)
-set_property(TARGET libvlccore PROPERTY
-IMPORTED_LOCATION "${CMAKE_SOURCE_DIR}/vlc/libvlccore.lib")
