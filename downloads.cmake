4276babf58726e91840451049de52917be73fa17
Ramazan Ergüder Bekrek <e.bekrek@yandex.com>
2021-05-26 08:29:06 +0200

    Removed old modules.

--- downloads.cmake
+++
@@ -1,9 +0,0 @@
-include(FetchContent)
-
-FetchContent_Declare(
-        vlc-qt
-        GIT_REPOSITORY "https://github.com/vlc-qt/vlc-qt"
-        GIT_TAG "1.1.1"
-)
-
-FetchContent_MakeAvailable(vlc-qt)
